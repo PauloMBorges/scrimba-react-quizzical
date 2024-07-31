affcd7334b8e956426341378c545c3fda43977e9
PauloMBorges <paulodemeloborges@gmail.com>
2024-07-31 17:27:58 -0300

    Create landing component with content

---
+++ src/components/Landing.jsx
@@ -0,0 +1,11 @@
+function Landing() {
+    return (
+        <div className="landing">
+            <h1 className="landing-title">Quizzical</h1>
+            <h2 className="landing-description">Some description if needed</h2>
+            <button className="start-quiz-btn">Start quiz</button>
+        </div>
+    )
+}
+
+export default Landing
