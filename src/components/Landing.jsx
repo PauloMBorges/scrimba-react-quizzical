feb30b4dd1e94f07e75d817430284aea83399e8b
PauloMBorges <paulodemeloborges@gmail.com>
2024-07-31 18:01:11 -0300

    Add and style blobs to landing page

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,11 +1,16 @@
+import blueBlob from "../assets/blob-blue.svg";
+import yellowBlob from "../assets/blob-yellow.svg";
+
 function Landing() {
-    return (
-        <div className="landing">
-            <h1 className="landing-title">Quizzical</h1>
-            <h2 className="landing-description">Some description if needed</h2>
-            <button className="start-quiz-btn">Start quiz</button>
-        </div>
-    )
+  return (
+    <div className="landing">
+      <h1 className="landing-title">Quizzical</h1>
+      <h2 className="landing-description">Test your knowledge! Ready?</h2>
+      <button className="start-quiz-btn">Start quiz</button>
+      <img src={blueBlob} alt="blue blob" className="blob blue-blob"/>
+        <img src={yellowBlob} alt="yellow blob" className="blob yellow-blob"/>
+    </div>
+  );
 }
 
-export default Landing
+export default Landing;
